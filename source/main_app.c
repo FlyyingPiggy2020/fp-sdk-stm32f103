68d2d70f731b7919c6ffd17cd6202b2adb47f407
FlyyingPiggy2020 <154562451@qq.com>
2024-07-20 17:04:07 +0800

    增加datacenter

--- source/main_app.c
+++ source/main_app.c
@@ -34,37 +34,26 @@ SOFTWARE.
 /*---------- includes ----------*/
 
 #include "main_app.h"
+#include "data_center.h"
 /*---------- macro ----------*/
 /*---------- type define ----------*/
 /*---------- variable prototype ----------*/
+data_center_t *center = NULL;
+account_t *key = NULL;
 /*---------- function prototype ----------*/
 /*---------- variable ----------*/
 /*---------- function ----------*/
-int used_size = 0;
-fp_timer_t *timer1;
-fp_timer_t *timer2;
-fp_timer_t *timer_next;
-extern int heap_init(void);
-extern int heap_get_used_size(void);
-void timer1_cb(fp_timer_t *task)
-{
-	log_a("hello world");
-}
-void timer2_cb(fp_timer_t *task)
-{
-	timer_next = fp_timer_get_next(timer1);
-	used_size = heap_get_used_size();
-}
+
 void main_app(void)
 {
 
-	heap_init();
-	_fp_timer_core_init();
-	timer1 = fp_timer_create(timer1_cb, 1000, NULL);
-	timer2 = fp_timer_create(timer2_cb, 1000, NULL);
-	fp_timer_set_repeat_count(timer1,5);
+    heap_init();
+    _fp_timer_core_init();
+	center = data_center_init("center");
+	key = account_init("key",center,0,NULL);
     while (1) {
-		fp_timer_handler();
+        fp_timer_handler();
     }
 }
+
 /*---------- end of file ----------*/
